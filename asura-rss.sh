7a2dd4d5359b7aefa7d456f80b98325bf32f3cfe
zen <71zenith@proton.me>
2023-11-11 21:50:15 +0530

    add: thumbnails

--- asura-rss.sh
+++ asura-rss.sh
@@ -3,23 +3,23 @@ base_url="https://asuratoon.com"
 
 oldfile="$(cat asura.xml)"
 
-time=$(date -u +%a,\ %d\ %b\ %Y\ %T\ %Z)
+time=$(date -R)
 
-! test -s asura.xml && printf '<?xml version="1.0" encoding="UTF-8" ?>\n<rss version="2.0">\n\n<channel>\n<title>AsuraScans (Scumbags) - RSS Feed</title>\n<link>https://github.com/71zenith/asura-rss</link>\n<description>A simple RSS feed for asurascans!</description>\n' >asura.xml
+! test -s asura.xml && printf '<?xml version="1.0" encoding="UTF-8" ?>\n<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">\n\n<channel>\n<title>AsuraScans (Scumbags) - RSS Feed</title>\n<link>https://github.com/71zenith/asura-rss</link>\n<description>A simple RSS feed for asurascans!</description>\n<atom:link href="https://raw.githubusercontent.com/71zenith/asura-rss/master/asura.xml" rel="self" type="application/rss+xml" />\n' >asura.xml
 
 to_search=$(cat manhwa)
 
-data=$(curl "$base_url" | tr -d '\n' | grep -Eo '<[^"]+"[^>]+><li><a href="[^"]+">Chapter [^>]+' | sed -nE 's|<h4>([^<]+)<.*href="([^"]+).*>([^<]+).*|\1\t\2\t\3|p')
+data=$(curl "$base_url" | sed '1,/Latest Update/d ; /Popular/,$d' | tr -d '\n' | sed 's/<\/div><div class="utao styletwo">/\n/g' | sed -nE 's|.*img src="([^"]+)".*<h4>([^>]+)</h4></a><ul class="([^"]+)"><li><a href="([^"]+)">([^<]+)<.*|\1\t\2\t\4\t\5|p')
 sed '/<\/channel>/d; /<\/rss>/d' -i asura.xml
 for i in $to_search; do
 	match=$(printf "%s\n" "$data" | grep "${i}-chapter")
 	if [ -n "$match" ]; then
-		title=$(printf "%s\n" "$match" | cut -f1)
-		link=$(printf "%s\n" "$match" | cut -f2)
-		chap=$(printf "%s\n" "$match" | cut -f3)
-		rss=$(printf '\n<item>\n<title>%s</title>\n<link>%s</link>\n<description>A simple RSS feed for asura!</description>\n<pubDate>%s</pubDate>\n</item>\n' "$title $chap" "$link" "${time}")
+		thumb=$(printf "%s\n" "$match" | cut -f1)
+		title=$(printf "%s\n" "$match" | cut -f2)
+		link=$(printf "%s\n" "$match" | cut -f3)
+		chap=$(printf "%s\n" "$match" | cut -f4)
+		rss=$(printf '\n<item>\n<title>%s</title>\n<link>%s</link>\n<description>A simple RSS feed for asura!</description>\n<enclosure url="%s"/>\n<pubDate>%s</pubDate>\n</item>\n' "$title $chap" "$link" "$thumb" "${time}")
 		if ! printf "%s\n" "$oldfile" | grep -q "$title $chap"; then
-			sed '/<\/channel>/d; /<\/rss>/d' -i asura.xml
 			printf "%s\n" "$rss" >>asura.xml
 		fi
 	fi
